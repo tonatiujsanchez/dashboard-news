33536a204db8c11a798369779dab7904f630d727
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2022-08-11 03:20:59 -0500

    Modelos actualizados

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 export { default as User } from "./User";
 export { default as Entry } from "./Entry";
 export { default as Category } from "./Category";
-export { default as Subcategory } from "./Subcategory";
 export { default as Author } from "./Author";
 export { default as Image } from "./Image";
